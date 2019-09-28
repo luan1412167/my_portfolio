ca310fb1d06e0ea9094806183f09456c6e96f270
luan.dao <luan.dao@eyeq.tech>
2019-09-29 00:25:40 +0700

    my_portfolio runable

---
+++ src/components/timeline.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-2">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>AI Reseach Engineer at VinID <span>09/2019-Present</span></h2>
+                        <p>I have joined VinID as a reseach engineer. In here I'm be charge of reseach to solve problems based on AI foundation. Then deploy and deliver to our customers.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>AI fresher at EYEQ Tech <span>10/2018-09/2019</span></h2>
+                        <p>I have joined as AI fresher. In here I have learned very much things about CNN model, RNN model as well as how to implement and deploy deep leaning model in production environment.Very much skills for engineer like NGINX, Docker, Socket IO, MongoDB. </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Elevator installation and maintenance engineer <span>2017-2018</span></h2>
+                        <p>I'm be in charge of install part of electricity for elevator. </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduation <span>2014-2018</span></h2>
+                        <p>I have learned and researched many things at my university. Something about Math, image processing, Artificial Intelligence.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}

2aec8d26b128e894dfe698c4e31d69cbce091399
luan.dao <luan.dao@eyeq.tech>
2019-09-29 00:36:51 +0700

    Updates

---
+++ precache-manifest.285b787da0c146eba1737342a72eaf2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7403a7d53f8c7fd6b5e1a2ef3d9ab0d3",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "0f48662496e31a02b582",
+    "url": "/my_portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "ad0d8a7bc983077502f3",
+    "url": "/my_portfolio/static/js/2.46c85f09.chunk.js"
+  },
+  {
+    "revision": "0f48662496e31a02b582",
+    "url": "/my_portfolio/static/js/main.f4044cd6.chunk.js"
+  },
+  {
+    "revision": "2d24fc74f9a1da9d8278",
+    "url": "/my_portfolio/static/js/runtime-main.bfd13320.js"
+  }
+]);
